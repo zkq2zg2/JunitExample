92d7a2cf8af287099b72309e790eb381036452a3
Parvez Misarwala <parvezmisarwala@gmail.com>
2017-04-27 14:43:18 +0530

    Update TestHelloWorld.java

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -27,6 +27,6 @@ public class TestHelloWorld {
 	   {
 	      h.setName("World");
 	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Hello World!updated");
+	      assertEquals(h.getMessage(),"Hello World!");
 	   }
 }
